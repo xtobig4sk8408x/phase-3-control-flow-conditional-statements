dfc12f16548c8c32c5c37444cba0a526aa8e0570
Theodore John Josef <john.josef@aol.com>
2023-01-09 01:14:10 -0800

    commit tree 62d5042e6ebd1cb082360dcc19cc7867dd5acaa4

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,39 @@
 def admin_login(username, password)
-  # your code here
+  ((username == 'admin' || username == 'ADMIN') && password == '12345') ? "Access granted" : "Access denied"
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  case 
+  when temperature < 40
+    "It's brisk out there!"
+  when temperature < 65
+    "It's a little chilly out there!"
+  when temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0 
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    return num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case
+  when operation == "+" then num1 + num2
+  when operation == "-" then num1 - num2
+  when operation == "*" then num1 * num2
+  when operation == "/" then num1 / num2
+  else puts "Invalid operation!"
+  end
 end
 
